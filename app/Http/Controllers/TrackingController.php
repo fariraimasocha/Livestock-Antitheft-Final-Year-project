8b2f869fd5d81ee5b8f17d44902b09bf040bdd29
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2024-03-27 12:14:36 +0200

    feat
    live streaming logic

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class TrackingController extends Controller
 {
     public function index()
     {
-        return view('tracking.index');
+        $client = new Client();
+        $url = 'http://192.168.81.13/cam-hi.jpg';
+
+        $response = $client->request('GET', $url);
+        $videoFeed = $response->getBody()->getContents();
+
+        return view('tracking.index',
+        [
+            'videoFeed' => $videoFeed
+        ]);
     }
 }
