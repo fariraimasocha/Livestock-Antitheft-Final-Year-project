0adab15e1693ac778092215ed589aa52b2fef5be
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2024-03-27 11:43:54 +0200

    feat
    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Livestock;
+use App\Models\Employee;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $users = User::all();
+        $obooks = Role::all();
+        $reports = Employee::all();
+        $roles = Role::all();
+        $permissions = Permission::all();
+        $assessments = Livestock::all();
+
         return view('dashboard.index');
     }
 }
